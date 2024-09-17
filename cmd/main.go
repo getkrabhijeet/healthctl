dc97b26e7e1e7e3ba7de465e3c7325aada30c941
Ameet Porwal <porwalameet@gmail.com>
2024-09-17 22:38:05 +0530

    UI panel changes for healthctl

--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,230 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"healthctl/pkg/k8s"
+	"log"
+	"strings"
+
+	"github.com/rivo/tview"
 )
 
-func main() {
-	client, err := k8s.CreateK8sClientSet()
-	if err != nil {
-		panic(err.Error())
+type testInfoUI struct {
+	ctx       context.Context
+	cancel    context.CancelFunc
+	app       *tview.Application
+	panel     *tview.Flex
+	testType  *tview.TextView
+	txAddress *tview.TableCell
+}
+
+var Logo = []string{
+
+	`.----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------. `,
+	`| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |`,
+	`| |  ____  ____  | || |  _________   | || |      __      | || |   _____      | || |  _________   | || |  ____  ____  | || |     ______   | || |  _________   | || |   _____      | |`,
+	`| | |_   ||   _| | || | |_   ___  |  | || |     /  \     | || |  |_   _|     | || | |  _   _  |  | || | |_   ||   _| | || |   .' ___  |  | || | |  _   _  |  | || |  |_   _|     | |`,
+	`| |   | |__| |   | || |   | |_  \_|  | || |    / /\ \    | || |    | |       | || | |_/ | | \_|  | || |   | |__| |   | || |  / .'   \_|  | || | |_/ | | \_|  | || |    | |       | |`,
+	`| |   |  __  |   | || |   |  _|  _   | || |   / ____ \   | || |    | |   _   | || |     | |      | || |   |  __  |   | || |  | |         | || |     | |      | || |    | |   _   | |`,
+	`| |  _| |  | |_  | || |  _| |___/ |  | || | _/ /    \ \_ | || |   _| |__/ |  | || |    _| |_     | || |  _| |  | |_  | || |  \ '.___ '\  | || |    _| |_     | || |   _| |__/ |  | |`,
+	`| | |____||____| | || | |_________|  | || ||____|  |____|| || |  |________|  | || |   |_____|    | || | |____||____| | || |   '._____.'  | || |   |_____|    | || |  |________|  | |`,
+	`| |              | || |              | || |              | || |              | || |              | || |              | || |              | || |              | || |              | |`,
+	`| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |`,
+	` '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------' `,
+}
+
+func createApplication() (app *tview.Application) {
+	app = tview.NewApplication()
+	pages := tview.NewPages()
+
+	infoUI := createInfoPanel(app)
+	logPanel := createTextViewPanel(app, "Log")
+
+	log.SetOutput(logPanel)
+
+	clusterList := getClusterList()
+	// the clusterList here will be retreived from KubeConfig command.
+	// For test purpose, this is hardcoded now.
+
+	clusterList.AddItem("Cluster 1", "", '1', nil)
+	clusterList.AddItem("Cluster 2", "", '2', nil)
+	clusterList.AddItem("Cluster 3", "", '3', nil)
+	clusterList.AddItem("Cluster 4", "", '4', nil)
+
+	commandList := createCommandList()
+	commandList.AddItem("K8s Sanity", "", 'k', sendCommand(pages, infoUI))
+	commandList.AddItem("Infra Sanity", "", 'i', sendCommand(pages, infoUI))
+	commandList.AddItem("PAAS Sanity", "", 'p', sendCommand(pages, infoUI))
+	commandList.AddItem("SMF Sanity", "", 'a', sendCommand(pages, infoUI))
+	commandList.AddItem("UPF Sanity", "", 'u', sendCommand(pages, infoUI))
+	commandList.AddItem("Storage Analysis", "", 's', sendCommand(pages, infoUI))
+	commandList.AddItem("Netpol Scan", "", 'n', sendCommand(pages, infoUI))
+	commandList.AddItem("Stop", "", 's', stop(infoUI))
+	commandList.AddItem("Quit", "", 'q', func() {
+		app.Stop()
+	})
+
+	reportList := createReportList()
+	reportList.AddItem("2024-17-09", "", 0, nil)
+	reportList.AddItem("2024-16-09", "", 0, nil)
+	reportList.AddItem("2024-15-09", "", 0, nil)
+
+	layout := createMainLayout(clusterList, commandList, reportList)
+	pages.AddPage("main", layout, true, true)
+
+	app.SetRoot(pages, true)
+
+	return app
+}
+
+func createMainLayout(clusterList, commandList tview.Primitive, reportsPanel tview.Primitive) (layout *tview.Flex) {
+	///// Main Layout /////
+	banner := tview.NewTextView()
+	banner.SetBorder(true)
+	banner.SetText(strings.Join(Logo, fmt.Sprintf("\n[%s::b]", "green")))
+	banner.SetTextAlign(tview.AlignLeft)
+	banner.SetDynamicColors(true)
+	banner.SetTextAlign(tview.AlignCenter)
+
+	mainLayout := tview.NewFlex().SetDirection(tview.FlexColumn).
+		AddItem(clusterList, 0, 30, true).
+		AddItem(commandList, 0, 30, true).
+		AddItem(reportsPanel, 0, 30, false)
+
+	info := tview.NewTextView()
+	info.SetBorder(true)
+	info.SetText("HealthCtl v1.0 - Copyright 2024 Microsoft Corp")
+	info.SetTextAlign(tview.AlignCenter)
+
+	layout = tview.NewFlex().SetDirection(tview.FlexRow).
+		AddItem(banner, 13, 1, false).
+		AddItem(mainLayout, 20, 30, true).
+		AddItem(info, 0, 1, false)
+
+	return layout
+}
+
+func sendCommand(pages *tview.Pages, infoUI *testInfoUI) func() {
+	return func() {
+		startFunc := func() {
+			stop(infoUI)()
+			pages.SwitchToPage("main")
+			pages.RemovePage("modal")
+			ctx, cancel := context.WithCancel(context.Background())
+			infoUI.ctx = ctx
+			infoUI.cancel = cancel
+			go func() {
+				defer func() {
+					cancel()
+					infoUI.ctx = nil
+				}()
+				fmt.Println("In sendCommand")
+			}()
+		}
+
+		cancelFunc := func() {
+			pages.SwitchToPage("main")
+			pages.RemovePage("modal")
+		}
+
+		form := tview.NewForm()
+		form.AddButton("Start", startFunc)
+		form.AddButton("Cancel", cancelFunc)
+		form.SetCancelFunc(cancelFunc)
+		form.SetButtonsAlign(tview.AlignCenter)
+
+		form.SetBorder(true).SetTitle(fmt.Sprintf("Send 0x%02X and Listen", "Sample test"))
+
+		modal := createModalForm(pages, form, 13, 55)
+
+		pages.AddPage("modal", modal, true, true)
+
 	}
-	fmt.Println("Client created")
+}
 
-	k8s.GetClusterhealth(client)
+func createInfoPanel(app *tview.Application) (infoUI *testInfoUI) {
+	///// Info /////
+	infoPanel := tview.NewFlex().SetDirection(tview.FlexRow)
 
-	// rook.GetRookClusterHealth(client)
+	infoUI = &testInfoUI{}
+	infoUI.app = app
+	infoUI.panel = infoPanel
 
-	// mongo.GetMongoClusterHealth(client)
+	infoUI.testType = tview.NewTextView()
+	infoUI.testType.SetBorder(true)
+	infoUI.testType.SetText("healthctl")
+	infoUI.testType.SetTextAlign(tview.AlignCenter)
+	infoPanel.AddItem(infoUI.testType, 0, 1, false)
 
-	// redis.GetRedisClusterHealth(client)
+	txInfo := tview.NewTable()
+	txInfo.SetBorder(true).SetTitle("Clusters")
 
-	// application.GetApplicationHealth(client)
+	txInfo.SetCellSimple(0, 0, "Clusters:")
+	txInfo.GetCell(0, 0).SetAlign(tview.AlignRight)
+	txInfo.SetCell(0, 1, infoUI.txAddress)
 
-	k8s.GetAPIResources(client)
+	infoInnerPanel := tview.NewFlex().SetDirection(tview.FlexColumn).
+		AddItem(txInfo, 0, 1, false)
+	infoPanel.AddItem(infoInnerPanel, 0, 1, false)
 
+	return infoUI
+}
+
+func createTextViewPanel(app *tview.Application, name string) (panel *tview.TextView) {
+	panel = tview.NewTextView()
+	panel.SetBorder(true).SetTitle(name)
+	panel.SetChangedFunc(func() {
+		app.Draw()
+	})
+	return panel
+}
+
+func stop(infoUI *testInfoUI) func() {
+	return func() {
+		if infoUI.cancel != nil {
+			infoUI.cancel()
+		}
+	}
+}
+
+func createCommandList() (commandList *tview.List) {
+	///// Commands /////
+	commandList = tview.NewList()
+	commandList.SetBorder(true).SetTitle("Operation")
+	commandList.ShowSecondaryText(false)
+	return commandList
+}
+func getClusterList() (clusterList *tview.List) {
+	///// Clusters /////
+	clusterList = tview.NewList()
+	clusterList.SetBorder(true).SetTitle("Clusters")
+	clusterList.ShowSecondaryText(false)
+	return clusterList
+}
+
+func createReportList() (reportList *tview.List) {
+	///// Reports /////
+	reportList = tview.NewList()
+	reportList.SetBorder(true).SetTitle("Reports")
+	reportList.ShowSecondaryText(false)
+	return reportList
+}
+
+func createModalForm(pages *tview.Pages, form tview.Primitive, height int, width int) tview.Primitive {
+	modal := tview.NewFlex().SetDirection(tview.FlexColumn).
+		AddItem(nil, 0, 1, false).
+		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
+			AddItem(nil, 0, 1, false).
+			AddItem(form, height, 1, true).
+			AddItem(nil, 0, 1, false), width, 1, true).
+		AddItem(nil, 0, 1, false)
+	return modal
+}
+
+func main() {
+	app := createApplication()
+
+	if err := app.Run(); err != nil {
+		panic(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module healthctl
 go 1.21.0
 
 require (
+	github.com/rivo/tview v0.0.0-20240818110301-fd649dbf1223
 	k8s.io/apimachinery v0.28.2
 	k8s.io/client-go v0.28.2
 )
@@ -10,6 +11,8 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.9.0 // indirect
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.7.1 // indirect
 	github.com/go-logr/logr v1.2.4 // indirect
 	github.com/go-openapi/jsonpointer v0.19.6 // indirect
 	github.com/go-openapi/jsonreference v0.20.2 // indirect
@@ -23,16 +26,19 @@ require (
 	github.com/imdario/mergo v0.3.6 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/net v0.13.0 // indirect
 	golang.org/x/oauth2 v0.8.0 // indirect
-	golang.org/x/sys v0.10.0 // indirect
-	golang.org/x/term v0.10.0 // indirect
-	golang.org/x/text v0.11.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/term v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
